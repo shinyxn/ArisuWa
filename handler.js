134bc88ff9770ef83e762e6cbbb43ad69a42be38
indonumberone <01muqsithbarru@gmail.com>
2023-10-10 00:32:29 +0700

    test prefix + siwtch case

--- handler.js
+++ handler.js
@@ -9,15 +9,37 @@ export default async function (sock, message) {
     (imageMessage && imageMessage.caption) ||
     (videoMessage && videoMessage.caption);
 
+  const reply = async (text) => {
+    await sock.sendMessage(senderNumber, { text }, { quoted: message });
+  };
+
+  let prefix = /^[\\/!#.]/gi.test(textMessage)
+    ? textMessage.match(/^[\\/!#.]/gi)
+    : "/";
+  const firstmess = textMessage.startsWith(prefix);
+  let pesan = textMessage
+    .replace(prefix, "")
+    .trim()
+    .split(/ +/)
+    .shift()
+    .toLowerCase();
+  if (firstmess) {
+    switch (pesan) {
+      case "oii":
+        {
+          reply("oii");
+        }
+        break;
+      case "pagi":
+        reply("pagi jugaa");
+        break;
+    }
+  }
   if (textMessage == "hola") {
-    await sock.sendMessage(
-      senderNumber,
-      { text: "Halo juga" },
-      { quoted: message }
-    );
+    await reply("haloo");
   }
 
   if (textMessage == "ip") {
-    ipaddr(sock, message, senderNumber)
+    ipaddr(sock, message, senderNumber);
   }
 }
