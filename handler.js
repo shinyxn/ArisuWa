28d0bec25519d75b8e0d2b3c80d85b1b95c30fc0
your_username <01muqsithbarru@gmail.com>
2023-10-10 19:32:44 +0700

    test jadwal

--- handler.js
+++ handler.js
@@ -6,8 +6,8 @@ export default async function (sock, message) {
     message.message.conversation ||
     (message.message.extendedTextMessage &&
       message.message.extendedTextMessage.text) ||
-    (imageMessage && imageMessage.caption) ||
-    (videoMessage && videoMessage.caption);
+    (message.imageMessage && message.imageMessage.caption) ||
+    (message.videoMessage && message.videoMessage.caption);
 
   const reply = async (text) => {
     await sock.sendMessage(senderNumber, { text }, { quoted: message });
@@ -16,31 +16,37 @@ export default async function (sock, message) {
   let prefix = /^[\\/!#.]/gi.test(textMessage)
     ? textMessage.match(/^[\\/!#.]/gi)
     : "/";
-  const firstmess = textMessage.startsWith(prefix);
-  let pesan = textMessage
-    .replace(prefix, "")
-    .trim()
-    .split(/ +/)
-    .shift()
-    .toLowerCase();
-  if (firstmess) {
-    switch (pesan) {
-      case "oii":
-        {
-          reply("oii");
-        }
-        break;
-      case "pagi":
-        reply("pagi jugaa");
-        break;
-    }
-  }
-  if (textMessage == "hola") {
-    var id = "6289649178812@s.whatsapp.net";
-    await sock.sendMessage(id, { text: "oh hello there" });
-  }
+  if (textMessage) {
+    const firstmess = textMessage.startsWith(prefix);
+    let pesan = textMessage
+      .replace(prefix, "")
+      .trim()
+      .split(/ +/)
+      .shift()
+      .toLowerCase();
 
-  if (textMessage == "ip") {
-    ipaddr(sock, message, senderNumber);
+    if (firstmess) {
+      switch (pesan) {
+        case "oii":
+          {
+            reply("oii");
+          }
+          break;
+        case "pagi":
+          reply("pagi jugaa");
+          break;
+      }
+    }
+
+    if (textMessage == "hola") {
+      var id = "6289649178812@s.whatsapp.net";
+      await sock.sendMessage(id, { text: "oh hello there" });
+    }
+
+    if (textMessage == "ip") {
+      ipaddr(sock, message, senderNumber);
+    }
+  } else {
+    console.error("wa desktop detected");
   }
 }
