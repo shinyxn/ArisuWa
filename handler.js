3b7b75c1d09d4428691489500fde0fb0b73ae8c4
Shinyxn <97039464+shinyxn@users.noreply.github.com>
2024-02-02 21:06:33 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -125,13 +125,11 @@ id room kamu: ${roomId}`,
               m.messages.forEach(async (message) => {
                 if (
                   message.message?.extendedTextMessage?.text || message.message?.conversation ||
-                  message.message?.extendedTextMessage?.contextInfo?.quotedMessage
-                    ?.conversation
+                  message.message?.extendedTextMessage?.contextInfo?.quotedMessage?.conversation
                 ) {
                   const text =
                     message.message?.extendedTextMessage?.text || message.message?.conversation ||
-                    message.message?.extendedTextMessage?.contextInfo?
-                      .quotedMessage?.conversation;
+                    message.message?.extendedTextMessage?.contextInfo?.quotedMessage?.conversation;
 
                   const me = message.key.fromMe;
                   if (menfess === true && me === false) {
