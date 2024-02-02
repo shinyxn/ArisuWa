b341975bb7f481495239c859fe55b4314071b456
shinyxn <97039464+shinyxn@users.noreply.github.com>
2024-02-02 20:36:46 +0700

    fix TypeError: Cannot read properties of null (reading 'text')

--- handler.js
+++ handler.js
@@ -1,18 +1,18 @@
-import {downloadMediaMessage, getContentType} from '@whiskeysockets/baileys';
-import getDikti from './commands/dikti.js';
-import {ipaddr} from './commands/ipaddr.js';
-import {tiktokdl} from './commands/Tiktokv1.js';
-import {TiktokV2} from './commands/Tiktokv2.js';
-import {makeid} from './lib/makeid.js';
-import fs from 'fs';
+import { downloadMediaMessage, getContentType } from "@whiskeysockets/baileys";
+import getDikti from "./commands/dikti.js";
+import { ipaddr } from "./commands/ipaddr.js";
+import { tiktokdl } from "./commands/Tiktokv1.js";
+import { TiktokV2 } from "./commands/Tiktokv2.js";
+import { makeid } from "./lib/makeid.js";
+import fs from "fs";
 // import menfess from './lib/Menfess.js';
 
-import {insertData} from './lib/mongoDB.js';
+import { insertData } from "./lib/mongoDB.js";
 //import { downloadMedia } from "./lib/download.js";
 
-import {Sticker, createSticker, StickerTypes} from 'wa-sticker-formatter';
-import {resolve} from 'path';
-import {promises} from 'dns';
+import { Sticker, createSticker, StickerTypes } from "wa-sticker-formatter";
+import { resolve } from "path";
+import { promises } from "dns";
 
 export default async function (sock, m) {
   const senderNumber = m.key.remoteJid;
@@ -39,50 +39,50 @@ export default async function (sock, m) {
 
     try {
       var body =
-        m.mtype === 'conversation'
+        m.mtype === "conversation"
           ? m.message.conversation
-          : m.mtype == 'imageMessage'
+          : m.mtype == "imageMessage"
           ? m.message.imageMessage.caption
-          : m.mtype == 'videoMessage'
+          : m.mtype == "videoMessage"
           ? m.message.videoMessage.caption ||
             m.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage
-          : m.mtype == 'extendedTextMessage'
+          : m.mtype == "extendedTextMessage"
           ? m.message.extendedTextMessage.text ||
             m.message.extendedTextMessage.contextInfo.quotedMessage.conversation
-          : m.mtype == 'ephemeralMessage'
+          : m.mtype == "ephemeralMessage"
           ? m.message.ephemeralMessage.message.extendedTextMessage.text
-          : m.mtype == 'buttonsResponseMessage'
+          : m.mtype == "buttonsResponseMessage"
           ? m.message.buttonsResponseMessage.selectedButtonId
-          : m.mtype == 'listResponseMessage'
+          : m.mtype == "listResponseMessage"
           ? m.message.listResponseMessage.singleSelectReply.selectedRowId
-          : m.mtype == 'templateButtonReplyMessage'
+          : m.mtype == "templateButtonReplyMessage"
           ? m.message.templateButtonReplyMessage.selectedId
-          : m.mtype === 'messageContextInfo'
+          : m.mtype === "messageContextInfo"
           ? m.message.buttonsResponseMessage?.selectedButtonId ||
             m.message.listResponseMessage?.singleSelectReply.selectedRowId ||
             m.text
-          : '';
+          : "";
     } catch (e) {
       console.log(e);
     }
   }
 
   const reply = async (text) => {
-    await sock.sendMessage(senderNumber, {text}, {quoted: m});
+    await sock.sendMessage(senderNumber, { text }, { quoted: m });
   };
 
   const sendVideo = async (url) => {
     await sock.sendMessage(
       senderNumber,
-      {caption: 'Nyo videone', video: {url: url}},
-      {quoted: m},
+      { caption: "Nyo videone", video: { url: url } },
+      { quoted: m }
     );
   };
   try {
-    let prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : '/';
+    let prefix = /^[\\/!#.]/gi.test(body) ? body.match(/^[\\/!#.]/gi) : "/";
     const firstmess = body.startsWith(prefix);
     let pesan = body
-      .replace(prefix, '')
+      .replace(prefix, "")
       .trim()
       .split(/ +/)
       .shift()
@@ -91,18 +91,18 @@ export default async function (sock, m) {
 
     if (firstmess) {
       switch (pesan) {
-        case 'menfess':
+        case "menfess":
           if (m.args.length < 1) {
-            reply('Usage: menfess 62xxx(number)');
+            reply("Usage: menfess 62xxx(number)");
             return;
           }
-          if (!m.args[0].includes('62')) {
-            reply('use 628xxx');
+          if (!m.args[0].includes("62")) {
+            reply("use 628xxx");
             return;
           }
           let menfess = true;
           const senderMenfess = senderNumber;
-          const receiverNumber = m.args[0] + '@s.whatsapp.net';
+          const receiverNumber = m.args[0] + "@s.whatsapp.net";
           // const message = m.args.slice(1).join(' ');
           let roomId = makeid(5);
 
@@ -121,54 +121,60 @@ id room kamu: ${roomId}`,
           }
 
           const endRoom = await new Promise(async (resolve) => {
-            sock.ev.on('messages.upsert', async (m) => {
+            sock.ev.on("messages.upsert", async (m) => {
               m.messages.forEach(async (message) => {
-                const text =
+                if (
                   message.message.extendedTextMessage.text ||
                   message.message.extendedTextMessage.contextInfo.quotedMessage
-                    .conversation;
+                    .conversation
+                ) {
+                  const text =
+                    message.message.extendedTextMessage.text ||
+                    message.message.extendedTextMessage.contextInfo
+                      .quotedMessage.conversation;
 
-                const me = message.key.fromMe;
-                if (menfess === true && me === false) {
-                  let q = message.message.extendedTextMessage.text;
-                  console.log(q);
+                  const me = message.key.fromMe;
+                  if (menfess === true && me === false) {
+                    let q = message.message.extendedTextMessage.text;
+                    // console.log(q);
 
-                  if (message.key.remoteJid == receiverNumber) {
-                    await sock.sendMessage(senderMenfess, {
-                      text: `Menfess : ${text}`,
-                    });
-                  } else if (message.key.remoteJid == senderMenfess) {
-                    await sock.sendMessage(receiverNumber, {
-                      text: `Menfess : ${text}`,
-                    });
+                    if (message.key.remoteJid == receiverNumber) {
+                      await sock.sendMessage(senderMenfess, {
+                        text: `Menfess : ${text}`,
+                      });
+                    } else if (message.key.remoteJid == senderMenfess) {
+                      await sock.sendMessage(receiverNumber, {
+                        text: `Menfess : ${text}`,
+                      });
+                    }
+
+                    message.key.remoteJid == receiverNumber &&
+                    q.includes(`stop ${roomId}`)
+                      ? ((menfess = false), resolve("chat room telah berakhir"))
+                      : message.key.remoteJid == senderMenfess &&
+                        q.includes(`stop ${roomId}`)
+                      ? ((menfess = false), resolve("chat room telah berakhir"))
+                      : "";
                   }
-
-                  message.key.remoteJid == receiverNumber &&
-                  q.includes(`stop ${roomId}`)
-                    ? ((menfess = false), resolve('chat room telah berakhir'))
-                    : message.key.remoteJid == senderMenfess &&
-                      q.includes(`stop ${roomId}`)
-                    ? ((menfess = false), resolve('chat room telah berakhir'))
-                    : '';
                 }
               });
             });
           });
-          await sock.sendMessage(senderMenfess, {text: endRoom});
-          await sock.sendMessage(receiverNumber, {text: endRoom});
+          await sock.sendMessage(senderMenfess, { text: endRoom });
+          await sock.sendMessage(receiverNumber, { text: endRoom });
 
           break;
 
-        case 'oii':
+        case "oii":
           {
-            reply('oii');
+            reply("oii");
           }
           break;
-        case 'pagi':
-          reply('pagi jugaa');
+        case "pagi":
+          reply("pagi jugaa");
           break;
-        case 'tt':
-        case 'tiktok':
+        case "tt":
+        case "tiktok":
           const tiktok2 = (url) => {
             try {
               TiktokV2(url);
@@ -182,34 +188,34 @@ id room kamu: ${roomId}`,
           }
 
           break;
-        case 'dikti':
-          reply(await getDikti(m.args.join(' ')));
-        case 'p':
+        case "dikti":
+          reply(await getDikti(m.args.join(" ")));
+        case "p":
           console.log(m);
           break;
-        case 's':
+        case "s":
           {
             // download the message
             const media = await downloadMediaMessage(
               m,
-              'buffer' || 'stream',
+              "buffer" || "stream",
               {},
               {
                 // logger,
                 // // pass this so that baileys can request a reupload of media
                 // // that has been deleted
                 // reuploadRequest: sock.updateMediaMessage
-              },
+              }
             );
 
             let jancok = new Sticker(media, {
-              pack: 'TRIBOT', // The pack name
-              author: 'SYAUQI MEME DEPOK ' + makeid(5), // The author name
+              pack: "TRIBOT", // The pack name
+              author: "SYAUQI MEME DEPOK " + makeid(5), // The author name
               type: StickerTypes.FULL, // The sticker type
-              categories: ['🤩', '🎉'], // The sticker category
+              categories: ["🤩", "🎉"], // The sticker category
               id: makeid(5), // The sticker id
               quality: 50, // The quality of the output file
-              background: '#FFFFFF00', // The sticker background color (only for full stickers)
+              background: "#FFFFFF00", // The sticker background color (only for full stickers)
             });
 
             const sticker = await jancok.toMessage();
@@ -220,19 +226,19 @@ id room kamu: ${roomId}`,
             console.log(senderNumber);
           }
           break;
-        case 'sgif':
-        case 'sgift':
+        case "sgif":
+        case "sgift":
           {
             let media =
-              `https://vihangayt.me/maker/text2gif?q=` + m.args.join(' ');
+              `https://vihangayt.me/maker/text2gif?q=` + m.args.join(" ");
             let jancok = new Sticker(media, {
-              pack: 'TRIBOT', // The pack name
-              author: 'SYAUQI MEME DEPOK ' + makeid(10), // The author name
+              pack: "TRIBOT", // The pack name
+              author: "SYAUQI MEME DEPOK " + makeid(10), // The author name
               type: StickerTypes.FULL, // The sticker type
-              categories: ['🤩', '🎉'], // The sticker category
+              categories: ["🤩", "🎉"], // The sticker category
               id: makeid(5), // The sticker id
               quality: 50, // The quality of the output file
-              background: '#FFFFFF00', // The sticker background color (only for full stickers)
+              background: "#FFFFFF00", // The sticker background color (only for full stickers)
             });
             const sticker = await jancok.toMessage();
             await sock.sendMessage(senderNumber, sticker, {
@@ -240,18 +246,18 @@ id room kamu: ${roomId}`,
             });
           }
           break;
-        case 'stext':
+        case "stext":
           {
             let media =
-              `https://vihangayt.me/maker/text2img?q=` + m.args.join(' ');
+              `https://vihangayt.me/maker/text2img?q=` + m.args.join(" ");
             let jancok = new Sticker(media, {
-              pack: 'TRIBOT', // The pack name
-              author: 'SYAUQI MEME DEPOK ' + makeid(10), // The author name
+              pack: "TRIBOT", // The pack name
+              author: "SYAUQI MEME DEPOK " + makeid(10), // The author name
               type: StickerTypes.FULL, // The sticker type
-              categories: ['🤩', '🎉'], // The sticker category
+              categories: ["🤩", "🎉"], // The sticker category
               id: makeid(5), // The sticker id
               quality: 50, // The quality of the output file
-              background: '#FFFFFF00', // The sticker background color (only for full stickers)
+              background: "#FFFFFF00", // The sticker background color (only for full stickers)
             });
             const sticker = await jancok.toMessage();
             await sock.sendMessage(senderNumber, sticker, {
@@ -259,28 +265,28 @@ id room kamu: ${roomId}`,
             });
           }
           break;
-        case 'upload':
-        case 'tourl':
-        case 'tolink':
+        case "upload":
+        case "tourl":
+        case "tolink":
           {
-            reply('Prosess');
+            reply("Prosess");
             const medias = await downloadMediaMessage(
               m,
-              'buffer' || 'stream',
+              "buffer" || "stream",
               {},
               {
                 // logger,
                 // // pass this so that baileys can request a reupload of media
                 // // that has been deleted
                 // reuploadRequest: sock.updateMediaMessage
-              },
+              }
             );
-            let media = await fs.writeFileSync('test', medias);
-            let {TelegraPh} = await import('./lib/uploader.js');
-            let anu = await TelegraPh('test');
+            let media = await fs.writeFileSync("test", medias);
+            let { TelegraPh } = await import("./lib/uploader.js");
+            let anu = await TelegraPh("test");
             console.log(anu);
             reply(anu);
-            fs.unlinkSync('test');
+            fs.unlinkSync("test");
           }
           break;
       }
